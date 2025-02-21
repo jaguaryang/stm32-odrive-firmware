3591732f60ccea3a3081a89ea6796d8527364a3b
Jack Yang <jack.v.yang@gmail.com>
2025-02-22 11:56:19 +1300

    tuprules

--- Firmware/Tuprules.lua
+++ Firmware/Tuprules.lua
@@ -1,7 +1,4 @@
-print("JACK", "Hello from Tuprules.lua")
 -- Prevent tup from running `Firmware/fibre-cpp/Tupfile.lua` when we run it in
 -- the `Firmware` folder.
 no_libfibre = tup.getconfig("BUILD_LIBFIBRE") != "true"
--- no_libfibre = true
-
-print("JACK", no_libfibre)
+print("JACK", "Hello from Tuprules.lua", no_libfibre)
